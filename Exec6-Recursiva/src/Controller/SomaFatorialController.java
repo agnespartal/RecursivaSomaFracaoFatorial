d75e91dd4a73d43c7163c0b78d9073c9575f1b4b
Agnes <95083660+agnespartal@users.noreply.github.com>
2022-08-21 19:24:52 -0300

    Update SomaFatorialController.java

--- Exec6-Recursiva/src/Controller/SomaFatorialController.java
+++ Exec6-Recursiva/src/Controller/SomaFatorialController.java
@@ -11,16 +11,19 @@ public class SomaFatorialController {
 		if (num == 1) {
 			return 1;
 		} else {
-			double fat = fatorial(num);
-			soma = soma + (1/fat);
+			double fat = fatorial(num); //chama a função para calcular o fatorial 
+			soma = soma + (1/fat); 
 			return soma + somaFracao(num - 1);
+			//faz a fração de 1 sobre o fatorial, utilizando a chamada da função para o calculo do fatorial, e decai em 1 a cada chamada. 
 		}
 	}
 	
 	public double fatorial (double num) {
+		//condição de parada quando o valor inicial for igual a 1
 		if ( num <= 1) {
 			return 1;
 		} else
 			return num * fatorial(num - 1);
+			//multiplica o valor inicial com ele mesmo menos 1 até chegar na condição de parada
 	}
 }
